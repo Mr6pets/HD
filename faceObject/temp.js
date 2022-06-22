41a2682237433084362f12704c86a434d04bf71e
la <alvispets@gmail.com>
2022-06-22 19:11:26 +0800

    添加知识点

---
+++ faceObject/temp.js
@@ -0,0 +1,53 @@
+/***************************************************/
+// let obj={name:"alvis", age:18};
+// let a={...obj,lang:"zh"}
+// console.log(a,"999")// { name: 'alvis', age: 18, lang: 'zh' }
+/***************************************************/
+
+/********************function的配置项变更****************************/
+// function setConfig(params){
+//     let config={
+//         type:'*.jpeg,*.png',
+//         size:20000
+//     }
+//     config={...config,...params};
+//     console.log(config,"alvis")
+// }
+// let config={
+//     type:"*.gif"
+// }
+// setConfig(config);//{ type: '*.gif', size: 20000 } alvis
+
+/************************************************/
+
+/********************************************/
+// let {name,age}={name:"alvis",age:18};
+// console.log(name,age);
+// console.log("姓名是"+name,"年龄是："+age);
+/********************************************/
+
+/*******************************************复杂的解构*******************************************/
+// let compObj={
+//     name:"alvis",
+//     lession:{
+//         title:"javascript"
+//     }
+// }
+// let {name,lession:{title}}=compObj;
+// console.log(name,title);//alvis javascript
+/**************************************************************************************/
+
+
+/*****************结构合并************/
+function creatElemnt(){
+
+}
+
+
+
+
+/********************************************/
+// let arr=["alvis","hello world"];
+// let [,a]=arr;
+// console.log(a);//hello world
+/********************************************/
