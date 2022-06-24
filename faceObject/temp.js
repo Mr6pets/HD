679bd0ae5549fb18143a52162b9f931910fa0999
la <alvispets@gmail.com>
2022-06-24 10:33:48 +0800

    add some info

--- faceObject/temp.js
+++ faceObject/temp.js
@@ -38,16 +38,50 @@
 /**************************************************************************************/
 
 
-/*****************结构合并************/
-function creatElemnt(){
+/*****************解构属性合并************/
+// function creatElemnt(options={}){
+//     let {width=200 , height= 200 , backgroundColor="red" }=options;
+//     console.log("alvis",width,height,backgroundColor);
+//     const div=document.createElement("div");
+//     div.style.width=width+"px";
+//     div.style.height=height+"px";
+//     div.style.backgroundColor=backgroundColor;
+//     document.body.appendChild(div);
+// }
+// creatElemnt({width:500,height:300,backgroundColor:"lightblue"});
+/***************************************************************************/
 
-}
+/*************************/
+// 例子：数组
+// let arr=["alvis","single"];
+// console.log(arr);
+// // 看自己的属性无法看父亲
+// console.log(mark=arr.hasOwnProperty("length"));//true;
+// console.log(mark=arr.hasOwnProperty("concat"));//false;
+// console.log('length' in arr);//true//这个方法检测自己和父亲(原型)
+// console.log('concat' in arr);//true//这个方法检测自己和父亲(原型)
 
+// 例子：对象
+// let a={name:"alvis"};
+// let b={url:"www.helloworld.com"};
+// Object.setPrototypeOf(a,b);//将b指定为a放父级
+// console.log(a.hasOwnProperty("url"))//false 无法查看父级
+// console.log('url' in a);//true in的方法可以查询父亲
 
-
-
+// 通过函数可以检测
+// function oss(options={}){
+//     if(!options.hasOwnProperty('host')){
+//         throw new Error("需要设置主机名")
+//     }else{
+//         console.log("有主机")
+//     }
+// }
+// oss({name:"alvis"})
 /********************************************/
 // let arr=["alvis","hello world"];
 // let [,a]=arr;
 // console.log(a);//hello world
 /********************************************/
+
+
+
